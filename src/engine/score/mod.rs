2cbb254876879726fd20cb0c5194a25c487f66c9
Fabien <fabien.mathieu92@gmail.com>
2025-10-04 09:26:39 +0200

    moved impl Note in its module

--- src/engine/score/mod.rs
+++ src/engine/score/mod.rs
@@ -1,4 +1,6 @@
 pub mod default_params;
+pub mod note;
+
 use crate::{
     app::NotesGroup,
     engine::waves::WaveType,
@@ -98,8 +100,6 @@ impl ChorusParams {
         }
     }
 }
-
-mod note;
 
 #[derive(Serialize, Deserialize, Clone, PartialEq)]
 pub enum Interval {
@@ -250,48 +250,5 @@ impl Sequence {
                     }
                 }
             });
-    }
-}
-
-impl note::Note {
-    pub fn draw(&self, context: &[NotesGroup], rng: &mut rand::prelude::ThreadRng) -> Self {
-        match &self.interval {
-            Interval::RDTempered(degree, base, octave) => {
-                let others = context
-                    .iter()
-                    .map(
-                        |NotesGroup {
-                             notes, tolerance, ..
-                         }| {
-                            notes.iter().filter(|n| {
-                                self.time < n.time + n.duration + tolerance.0
-                                    && n.time < self.time + self.duration + tolerance.1
-                            })
-                        },
-                    )
-                    .flatten()
-                    // Proper interval overlap test:
-                    .filter_map(|n| {
-                        if let Interval::Tempered(d, _) = n.interval {
-                            Some(d)
-                        } else {
-                            None
-                        }
-                    })
-                    .collect::<Vec<_>>();
-
-                let seed = *others.choose(rng).unwrap_or(&0);
-                // let degree =
-                //     (((0..*degree).fold(seed, |acc, _| acc + base.choose(rng).unwrap()) % 12) + 12)
-                //         % 12;
-                let degree = (0..*degree).fold(seed, |acc, _| acc + base.choose(rng).unwrap()) % 12;
-
-                Self {
-                    interval: Interval::Tempered(degree, *octave),
-                    ..*self
-                }
-            }
-            _ => self.clone(),
-        }
     }
 }
--- src/engine/score/note.rs
+++ src/engine/score/note.rs
@@ -1,8 +1,9 @@
+use rand::seq::SliceRandom;
 use serde::Deserialize;
 
 use super::Interval;
 
-use crate::time_freq::Time;
+use crate::{app::NotesGroup, time_freq::Time};
 
 #[derive(Deserialize, Clone)]
 pub struct Note {
@@ -10,4 +11,46 @@ pub struct Note {
     pub duration: Time,
     pub interval: Interval,
     pub volume: f64,
+}
+impl Note {
+    pub fn draw(&self, context: &[NotesGroup], rng: &mut rand::prelude::ThreadRng) -> Self {
+        match &self.interval {
+            Interval::RDTempered(degree, base, octave) => {
+                let others = context
+                    .iter()
+                    .map(
+                        |NotesGroup {
+                             notes, tolerance, ..
+                         }| {
+                            notes.iter().filter(|n| {
+                                self.time < n.time + n.duration + tolerance.0
+                                    && n.time < self.time + self.duration + tolerance.1
+                            })
+                        },
+                    )
+                    .flatten()
+                    // Proper interval overlap test:
+                    .filter_map(|n| {
+                        if let Interval::Tempered(d, _) = n.interval {
+                            Some(d)
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                let seed = *others.choose(rng).unwrap_or(&0);
+                // let degree =
+                //     (((0..*degree).fold(seed, |acc, _| acc + base.choose(rng).unwrap()) % 12) + 12)
+                //         % 12;
+                let degree = (0..*degree).fold(seed, |acc, _| acc + base.choose(rng).unwrap()) % 12;
+
+                Self {
+                    interval: Interval::Tempered(degree, *octave),
+                    ..*self
+                }
+            }
+            _ => self.clone(),
+        }
+    }
 }
