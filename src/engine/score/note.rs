c24f4db99fd08115d9a64eb5798114e0d5fe108c
Fabien <fabien.mathieu92@gmail.com>
2025-10-20 16:24:14 +0200

    distinguish overlap when harmonising

--- src/engine/score/note.rs
+++ src/engine/score/note.rs
@@ -27,23 +27,32 @@ impl Note {
                         |NotesGroup {
                              notes, tolerance, ..
                          }| {
-                            notes.iter().filter(|n| {
-                                self.time < n.time + n.duration + tolerance.0
-                                    && n.time < self.time + self.duration + tolerance.1
-                            })
+                            notes
+                                .iter()
+                                .filter(|n| {
+                                    self.time < n.time + n.duration + tolerance.0
+                                        && n.time < self.time + self.duration + tolerance.1
+                                })
+                                .map(|n| {
+                                    (
+                                        n,
+                                        self.time < n.time + n.duration
+                                            && n.time < self.time + self.duration,
+                                    )
+                                })
                         },
                     )
                     .flatten()
-                    .filter_map(|n| {
+                    .filter_map(|(n, overlap)| {
                         if let Interval::Tempered(d, _) = n.interval {
-                            Some(d)
+                            Some((d, overlap))
                         } else {
                             None
                         }
                     })
                     .collect::<Vec<_>>();
 
-                let seed = *others.choose(rng).unwrap_or(&0);
+                let seed = others.choose(rng).unwrap_or(&(0, false)).0;
 
                 let degree = if harmonise {
                     (0..12)
@@ -62,19 +71,70 @@ impl Note {
         }
     }
 }
-fn tension(ns: impl Iterator<Item = i32> + Clone, d: i32) -> u32 {
-    ns.into_iter().map(|n| tension2(n, d)).sum()
+fn tension(ns: impl Iterator<Item = (i32, bool)> + Clone, d: i32) -> u32 {
+    ns.into_iter()
+        .map(|(n, overlap)| tension2(n, d, overlap))
+        .sum()
 }
-fn tension2(n1: i32, n2: i32) -> u32 {
+fn tension2(n1: i32, n2: i32, overlap: bool) -> u32 {
     let d = dist12(n1, n2);
     match d {
-        0 => 10,
-        1 => 11,
-        2 => 9,
-        3 => 4,
-        4 => 3,
-        5 => 0,
-        6 => 8,
+        // 0 => 10,
+        // 1 => 11,
+        // 2 => 9,
+        // 3 => 4,
+        // 4 => 3,
+        // 5 => 0,
+        // 6 => 8,
+        0 => {
+            if overlap {
+                1
+            } else {
+                10
+            }
+        }
+        1 => {
+            if overlap {
+                11
+            } else {
+                3
+            }
+        }
+        2 => {
+            if overlap {
+                9
+            } else {
+                2
+            }
+        }
+        3 => {
+            if overlap {
+                3
+            } else {
+                5
+            }
+        }
+        4 => {
+            if overlap {
+                3
+            } else {
+                4
+            }
+        }
+        5 => {
+            if overlap {
+                0
+            } else {
+                3
+            }
+        }
+        6 => {
+            if overlap {
+                8
+            } else {
+                6
+            }
+        }
         // 0 => 125,
         // 1 => 101,
         // 2 => 99,
